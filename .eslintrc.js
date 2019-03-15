5f349988e9e754417f2139329dc2235eaa847400
Do Thanh Phong <phongdt@mti-tech.vn>
2019-03-15 11:34:43 +0700

    ie support lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-      node: true
-  },
-  extends: [
-      'plugin:vue/essential',
-      '@vue/airbnb',
-      '@vue/typescript'
-  ],
-  rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      indent: ['error', 4],
-      'max-len': ['error', 500],
-      'no-plusplus': ['off'],
-      'class-methods-use-this': ['off'],
-      'comma-dangle': ['error', 'never'],
-      'func-names': ['error', 'as-needed']
-  },
-  parserOptions: {
-      parser: '@typescript-eslint/parser'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/airbnb',
+        '@vue/typescript'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent: ['error', 4],
+        'max-len': ['error', 500],
+        'no-plusplus': ['off'],
+        'class-methods-use-this': ['off'],
+        'comma-dangle': ['error', 'never'],
+        'func-names': ['error', 'as-needed']
+    },
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    }
 };
